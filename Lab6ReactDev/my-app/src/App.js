9bb3b8dcf6cc5681082123f7395404a50af63d44
mieczkowskimichal <21191@student.ans-elblag.pl>
2025-12-05 17:51:59 +0100

    lab6
    lab 6

---
+++ Lab6ReactDev/my-app/src/App.js
@@ -0,0 +1,19 @@
+import Hello from './Hello'; 
+import ToggleDetails from './ToggleDetails';
+import ScoreDisplay from './ScoreDisplay';
+import TaskList from './TaskList';
+import UserList from './UserList';
+import TimerCounter from './TimerCounter';
+function App() { 
+return ( 
+<div> 
+<Hello /> 
+<ToggleDetails/>
+<ScoreDisplay/>
+<TaskList/>
+<UserList/>
+<TimerCounter/>
+</div> 
+); 
+} 
+export default App;
