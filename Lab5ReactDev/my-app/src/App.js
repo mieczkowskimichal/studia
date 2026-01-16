fb631477532dbe201501e2ee6518d95cc979ba0a
mieczkowskimichal <21191@student.ans-elblag.pl>
2026-01-16 12:46:25 +0100

    poprawiony lab 5 i wstawiony build

--- Lab5ReactDev/my-app/src/App.js
+++ Lab5ReactDev/my-app/src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import InputTracker from './InputTracker.js';
 import { Hello, HelloWithProps } from './Hello';
 import Counter from './Counter.js';
+import LoginStatus from './LoginStatus.js';
+import TodoList from './TodoList.js';
+import LoginForm from './LoginForm.js';
 function App() {
+  const tasks = ["Zadanie 1", "Zadanie 2", "Zadanie 3"];
   return (
     <div className="App">
       <header className="App-header">
@@ -29,7 +33,13 @@ function App() {
 <HelloWithProps name="Celina" /> 
 
 <Counter></Counter>
+<InputTracker></InputTracker>
+<LoginStatus isLoggedIn={true} />
+<LoginStatus isLoggedIn={false} />
 
+
+<TodoList todos={tasks} />
+<LoginForm></LoginForm>
 </div>
 
       </header>
